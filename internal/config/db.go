af334da857dd41d59af94199b3572d11c96bc9b5
Devansh Soni <devanshsoni899@gmail.com>
2025-12-12 20:29:14 +0530

    feat: DB connecting

---
+++ internal/config/db.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	// get creds from env
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	// dsn format: user:password@tcp(host:port)/dbname?parseTime=true
+	// DB connection
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		user, pass, host, port, name)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("❌ Failed to connect to database: %v", err)
+	}
+
+	log.Println("Connected to MySQL Database!")
+}
